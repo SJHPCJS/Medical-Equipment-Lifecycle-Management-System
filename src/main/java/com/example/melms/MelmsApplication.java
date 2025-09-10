6a238b8e15a231c4cedf49700253b9e228e0aa64
oritention-geekkid <1285380930@qq.com>
2025-09-10 20:02:04 +0800

    initial commit

---
+++ src/main/java/com/example/melms/MelmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.melms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MelmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/melms/MelmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.melms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
